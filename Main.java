1b5d69f7c445096878265b8176c040c6c67466dd
joanossoubita23 <joanossoubita@yahoo.fr>
2023-12-13 20:55:44 -0500

    new all

--- Main.java
+++ Main.java
@@ -2,6 +2,14 @@ package ecomProject;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("""
+                Welcome to our Clothing Company
+                Please Enter a Number
+                1- Add a Clothes
+                2-Display Clothes
+                3-Update Clothes
+                4
+                """);
 
     }
 }
