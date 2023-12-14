5627bfb591ddeb188129a3f7a93156d5b38ef258
joanossoubita23 <joanossoubita@yahoo.fr>
2023-12-13 21:06:02 -0500

    implimentation of service

---
+++ ClothService.java
@@ -0,0 +1,11 @@
+package ecomProject;
+
+//in this method we will use basically some serices such as add display and other
+
+public interface ClothService {
+    void addClothes();
+    void displayClothes();
+    void UpdateClothes();
+    void  RemoveClothes();
+
+}
--- Main.java
+++ Main.java
@@ -3,13 +3,15 @@ package ecomProject;
 public class Main {
     public static void main(String[] args) {
         System.out.println("""
-                Welcome to our Clothing Company
+                Welcome to our Clothing Company👔
                 Please Enter a Number
                 1- Add a Clothes
                 2-Display Clothes
                 3-Update Clothes
-                4
+                4-Remove Clothes 
+                5-Exit 
                 """);
+
 
     }
 }
