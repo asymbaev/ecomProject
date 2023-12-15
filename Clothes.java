d6f9cacba2cc2b897c8782ce4f1b7f54d2e2b9d6
joanossoubita23 <joanossoubita@yahoo.fr>
2023-12-14 20:01:13 -0500

    i had put some changes inside the men

--- Clothes.java
+++ Clothes.java
@@ -1,38 +1,38 @@
 package ecomProject;
 
 public class Clothes {
-    private  String name;
-    private int id;
-    private double price;
+    private  String[] name;
+    private int[] id;
+    private double[] price;
 
     public Clothes(String name, int id, double price) {
-        this.name = name;
-        this.id = id;
-        this.price = price;
+        this.name = new String[]{name};
+        this.id = new int[]{id};
+        this.price = new double[]{price};
     }
 
-    public String getName() {
+    public String[] getName() {
         return name;
     }
 
     public void setName(String name) {
-        this.name = name;
+        this.name = new String[]{name};
     }
 
-    public int getId() {
+    public int[] getId() {
         return id;
     }
 
     public void setId(int id) {
-        this.id = id;
+        this.id = new int[]{id};
     }
 
-    public double getPrice() {
+    public double[] getPrice() {
         return price;
     }
 
     public void setPrice(double price) {
-        this.price = price;
+        this.price = new double[]{price};
     }
 
     @Override
--- MenClothes.java
+++ MenClothes.java
@@ -1,7 +1,41 @@
 package ecomProject;
 
 public class MenClothes extends Clothes{
+    private String jeans;
+    private String Costums;
+
+    public MenClothes(String name, int id, double price, String jeans, String costums) {
+        super(name, id, price);
+        this.jeans = jeans;
+        Costums = costums;
+    }
+
     public MenClothes(String name, int id, double price) {
         super(name, id, price);
+
+    }
+
+    public String getJeans() {
+        return jeans;
+    }
+
+    public void setJeans(String jeans) {
+        this.jeans = jeans;
+    }
+
+    public String getCostums() {
+        return Costums;
+    }
+
+    public void setCostums(String costums) {
+        Costums = costums;
+    }
+
+    @Override
+    public String toString() {
+        return "MenClothes{" +
+                "jeans='" + jeans + '\'' +
+                ", Costums='" + Costums + '\'' +
+                "} " + super.toString();
     }
 }
