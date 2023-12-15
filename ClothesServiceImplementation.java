49f2483a2852d2bb4ec63884734d9cc624058b93
joanossoubita23 <joanossoubita@yahoo.fr>
2023-12-14 20:51:55 -0500

    new fact

--- ClothesServiceImplementation.java
+++ ClothesServiceImplementation.java
@@ -1,9 +1,20 @@
 package ecomProject;
 
 public class ClothesServiceImplementation implements ClothService {
+    private final String Clothes[];
+    private final int Maxsize;
+    private int size;
+
+    public ClothesServiceImplementation(String[] clothes, int maxsize, int size) {
+        this.Clothes = new String[]{clothes[maxsize]};
+        Maxsize = maxsize;
+        this.size = size;
+    }
+
+
     @Override
     public void addClothes() {
-        addClothes();
+
 
     }
 
