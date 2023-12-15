0ce233a1b58ae20267d96cfe2b3f2b2bd023fdf4
joanossoubita23 <joanossoubita@yahoo.fr>
2023-12-14 20:03:16 -0500

    added an implementation class

---
+++ ClothesServiceImplementation.java
@@ -0,0 +1,23 @@
+package ecomProject;
+
+public class ClothesServiceImplementation implements ClothService {
+    @Override
+    public void addClothes() {
+
+    }
+
+    @Override
+    public void displayClothes() {
+
+    }
+
+    @Override
+    public void UpdateClothes() {
+
+    }
+
+    @Override
+    public void RemoveClothes() {
+
+    }
+}
